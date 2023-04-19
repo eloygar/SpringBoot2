e3d0b93c2b62663d313757632d763ff296bb6457
Eloygar <loygar2001@gmail.com>
2023-04-19 09:43:01 +0200

    6 commit

--- src/main/java/com/cebem/RickAndMorty/controllers/RickController.java
+++ src/main/java/com/cebem/RickAndMorty/controllers/RickController.java
@@ -4,15 +4,22 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
+import java.util.Set;
 
 import org.apache.el.util.MessageFactory;
 import org.apache.logging.log4j.message.Message;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +28,7 @@ import com.cebem.RickAndMorty.utils.Utils;
 import jakarta.websocket.server.PathParam;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class RickController {
     @GetMapping("/")
     public String saluda() {
@@ -72,6 +80,102 @@ public class RickController {
         boolean result = Utils.remove("datos.txt");
         return result ? "borrado corecto" : "borrado incorrecto";
 
-    } 
+    }
+
+    @GetMapping("/mayor")
+    public String mayor(@RequestParam String num1, @RequestParam String num2, @RequestParam String num3) {
+        try {
+            float numero1 = Float.parseFloat(num1);
+            float numero2 = Float.parseFloat(num2);
+            float numero3 = Float.parseFloat(num3);
+
+            // Comparar los números para encontrar el mayor
+            float mayor = numero1;
+            if (numero2 > mayor) {
+                mayor = numero2;
+            }
+            if (numero3 > mayor) {
+                mayor = numero3;
+            }
+
+            return "El mayor número es: " + mayor;
+
+        } catch (NumberFormatException e) {
+            return "ERROR: Alguno de los elementos no es un número válido";
+        }
+    }
+
+    @GetMapping("/capitalizar")
+    public String capitalizar(@RequestParam String frase) {
+        String[] palabras = frase.split("\\s+");
+        StringBuilder resultado = new StringBuilder();
+        for (String palabra : palabras) {
+            if (!palabra.isEmpty()) {
+                String primeraLetra = palabra.substring(0, 1).toUpperCase();
+                String restoPalabra = palabra.substring(1).toLowerCase();
+                resultado.append(primeraLetra).append(restoPalabra).append(" ");
+            }
+        }
+        return resultado.toString().trim();
+    }
+
+    @GetMapping("/info")
+    public String obtenerInformacion() {
+        try {
+            String informacion = Utils.readFromDisk("datos.txt");
+            return informacion;
+        } catch (IOException e) {
+            return "Error al intentar leer el archivo";
+        }
+    }
+
+    @DeleteMapping("/emptyFile")
+    public String emptyFile() {
+        try {
+            Utils.emptyFile("datos.txt");
+        } catch (IOException e) {
+            return "Error al intentar vaciar el fichero";
+        }
+        return "El fichero ha sido vaciado correctamente";
+    }
+
+    @RequestMapping("/vaciarFile")
+    public String vaciarFile() {
+        try {
+            Utils.writeOnDisk("datos.txt", "");
+        } catch (IOException e) {
+            return "Error al intentar vaciar el fichero";
+        }
+        return "El fichero ha sido vaciado correctamente";
+    }
+
+    @GetMapping("/randomColors")
+    public String[] randomColors() {
+        String[] coloresBasicos = { "negro", "azul", "marrón", "gris", "verde", "naranja", "rosa", "púrpura", "rojo",
+                "blanco", "amarillo" };
+        String[] coloresAleatorios = new String[3];
+
+        Random random = new Random();
+
+        for (int i = 0; i < 3; i++) {
+            int indiceAleatorio;
+            do {
+                indiceAleatorio = random.nextInt(coloresBasicos.length);
+            } while (contains(coloresAleatorios, coloresBasicos[indiceAleatorio]));
+
+            coloresAleatorios[i] = coloresBasicos[indiceAleatorio];
+        }
+
+        return coloresAleatorios;
+    }
+
+    private boolean contains(String[] array, String value) {
+        for (String item : array) {
+            if (item != null && item.equals(value)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
--- src/main/java/com/cebem/RickAndMorty/utils/Utils.java
+++ src/main/java/com/cebem/RickAndMorty/utils/Utils.java
@@ -1,6 +1,8 @@
 package com.cebem.RickAndMorty.utils;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -31,4 +33,27 @@ public class Utils {
         return result;
 
     }
+    public static String readFromDisk(String fileName) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(fileName)); // Abrir el archivo en modo de lectura
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line); // Leer cada línea del archivo y agregarla al StringBuilder
+            }
+        } finally {
+            if (br != null) {
+                br.close(); // Cerrar el BufferedReader
+            }
+        }
+        return sb.toString(); // Devolver el contenido del archivo como una cadena de texto
+    }
+
+    public static void emptyFile(String archivoPath) throws IOException {
+        PrintWriter writer = new PrintWriter(new FileWriter(archivoPath));
+        writer.print("");
+        writer.close();
+    }
+
 }
