28f0cd90e9a2a359056da5ad6aa79ee3c7765d84
Eloygar <loygar2001@gmail.com>
2023-04-24 17:09:53 +0200

    12 comnmit

--- src/main/java/com/cebem/RickAndMorty/controllers/WebController.java
+++ src/main/java/com/cebem/RickAndMorty/controllers/WebController.java
@@ -1,17 +1,30 @@
 package com.cebem.RickAndMorty.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.cebem.RickAndMorty.models.CharactersModel;
+import com.cebem.RickAndMorty.services.RickAndMortyService;
+
 @Controller
 public class WebController {
-    @RequestMapping("/rickandmorty/allTemplates")
 
-    public String charactersTemplates(Model modelo){
-        
+    @Autowired
+    RickAndMortyService rickAndMortyService;
+
+    @RequestMapping("/rickandmorty/allTemplates")
+    public String charactersTemplates(Model modelo) {
+
+        CharactersModel charactersModel = rickAndMortyService.getAllCharacter();
+
+        modelo.addAttribute("creator", "creado por Eloy");
+        modelo.addAttribute("characters", charactersModel.results);
+
         return "rickandmortyall";
-       }
+    }
 
 }
